e503f755c610ea480a67376200195fe68ab18e21
cl0ne <cl0ne@mithril.org.ua>
2016-04-23 16:25:06 +0300

    Register birth note models at admin site
    see #8

--- registry/vital_records/admin.py
+++ registry/vital_records/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import RegistryUser
+from .models import (RegistryUser, Person, Registrar, Residence, ApplicantInfo,
+                     BirthNote, BirthEvidence, BirthNoteLaw, BirthPlace)
+
 admin.site.register(RegistryUser, UserAdmin)
+
+admin.site.register(Person)
+admin.site.register(ApplicantInfo)
+admin.site.register(Residence)
+admin.site.register(Registrar)
+admin.site.register(BirthNote)
+admin.site.register(BirthPlace)
+admin.site.register(BirthEvidence)
+admin.site.register(BirthNoteLaw)
